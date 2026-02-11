387774a24d2c4d1459edb0617dddb159edad89b4
hemant sehrawat <hemantsehrawatjaat156.com>
2026-02-11 13:19:31 +0530

    `Added Solution class for anagram check and matrix generation`

--- index.py
+++ index.py
@@ -158,18 +158,81 @@ from ast import List
     
 # Anagram Check
 
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+# class Solution:
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     if len(s) != len(t):
+    #         return False
         
-        count_s = {}
-        count_t = {}
+    #     count_s = {}
+    #     count_t = {}
         
-        for char in s:
-            count_s[char] = count_s.get(char, 0) + 1
+    #     for char in s:
+    #         count_s[char] = count_s.get(char, 0) + 1
             
-        for char in t:
-            count_t[char] = count_t.get(char, 0) + 1
+    #     for char in t:
+    #         count_t[char] = count_t.get(char, 0) + 1
             
-        return count_s == count_t  
+    #     return count_s == count_t  
+
+# n number of matrix
+
+# class Solution:
+#     def generateMatrix(self, n: int) -> List[List[int]]:
+#         matrix = [[0] * n for _ in range(n)]
+#         num = 1
+#         left, right, top, bottom = 0, n - 1, 0, n - 1
+        
+#         while left <= right and top <= bottom:
+#             for i in range(left, right + 1):
+#                 matrix[top][i] = num
+#                 num += 1
+#             top += 1
+            
+#             for i in range(top, bottom + 1):
+#                 matrix[i][right] = num
+#                 num += 1
+#             right -= 1
+            
+#             if top <= bottom:
+#                 for i in range(right, left - 1, -1):
+#                     matrix[bottom][i] = num
+#                     num += 1
+#                 bottom -= 1
+            
+#             if left <= right:
+#                 for i in range(bottom, top - 1, -1):
+#                     matrix[i][left] = num
+#                     num += 1
+#                 left += 1
+        
+#         return matrix
+
+m = int(input("Enter a number: "))
+n = int(input("Enter another number: "))
+
+matrix = [[0] * n for _ in range(m)]
+num = 1
+left, right, top, bottom = 0, n - 1, 0, m - 1
+while left <= right and top <= bottom:
+    for i in range(left, right + 1):
+        matrix[top][i] = num
+        num += 1
+    top += 1
+    
+    for i in range(top, bottom + 1):
+        matrix[i][right] = num
+        num += 1
+    right -= 1
+    
+    if top <= bottom:
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+    
+    if left <= right:
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num += 1
+        left += 1
+        
