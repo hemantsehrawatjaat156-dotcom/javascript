0eca0d5b018d703185a095a2bfaab413b796fcbc
hemant sehrawat <hemantsehrawatjaat156.com>
2026-04-29 15:00:57 +0530

    bhbhu

---
+++ f3/src/search.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function Search() {
+    const [query, setQuery] = useState('');
+    useEffect(() => {
+       const getdata = () => {
+        console.log('Fetching data for query:', query);
+        // Simulate an API call
+        setTimeout(() => {
+            console.log('Data fetched for query:', query);
+        }, 1000);
+       }
+    }, [query]);
+
+    return (
+        <div>
+            <input type='text' placeholder='Search' value={search} onChange={(e) => setQuery(e.target.value)} />
+        </div>
+    );
+}   
+export default Search;
