88d9b0e4d8810c48736d0b187fa36a7fa1f98dbd
hemant sehrawat <hemantsehrawatjaat156.com>
2026-09-03 14:42:25 +0530

    new start

---
+++ new.py
@@ -0,0 +1,18 @@
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+
+# print(factorial(5))
+
+# def power(n, p):
+#     if p == 0:
+#         return 1
+#     else:
+#         return n * power(n, p - 1)
+
+# print(power(2, 3))
+
+
+
