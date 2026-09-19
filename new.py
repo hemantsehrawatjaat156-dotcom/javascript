b584c4e67b38931ed26cc72e3ff9d48c6fa1debd
hemant sehrawat <hemantsehrawatjaat156.com>
2026-09-20 04:57:27 +0530

    new

--- new.py
+++ new.py
@@ -34,3 +34,40 @@
 #     if i == 4:
 #         continue
 #     i -= 1
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print("i is no longer less than 6")
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+# for x in "banana":
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+# for x in range(6, 30, 4):
+#     print(x)
+
+# for x in range(6):
+#     print(x)
+#     if x == 3:
+#         break
+# else:
+#     print("Finally finished!")
+
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
+
+for x in []:
+    pass
