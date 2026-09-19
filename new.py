284441f3a19bb6af5654cd746ae578737b77c9ff
hemant sehrawat <hemantsehrawatjaat156.com>
2026-09-19 16:10:09 +0530

    nes

--- new.py
+++ new.py
@@ -13,6 +13,17 @@
 #         return n * power(n, p - 1)
 
 # print(power(2, 3))
-
-
-
+# month = 8
+# day = 4
+# match day:
+#     # case 1 | 2 | 3  | 5  | 7:
+    #     print("It's a weekday")
+    #     print("Welcome")
+    # case 4:
+    #     print("It's a welcome day")
+    # case _:
+    #     print("It's a weekend")
+    # case 1 | 2 | 3 | 4 | 5 if month <= 5:
+    #     print("It's a weekday in summer")
+    # case 1 | 2 | 3 | 4 | 5 if month <= 10:
+    #     print("It's a weekday in winter")
