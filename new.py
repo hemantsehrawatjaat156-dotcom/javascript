32852d75db2862fc733bdfb9e1bc23591947dbf8
hemant sehrawat <hemantsehrawatjaat156.com>
2026-09-21 21:35:18 +0530

    i find new thinks

--- new.py
+++ new.py
@@ -69,5 +69,52 @@
 #     for y in fruits:
 #         print(x, y)
 
-for x in []:
-    pass
+# for x in []:
+#     pass
+
+# def my_function():
+#     print("hello  from a function")
+
+# my_function()
+
+# weight = 10
+
+# def fruits_Price(Fruit):
+#     fruits_Price = Fruit * 10 * weight
+#     return fruits_Price
+
+# print(fruits_Price(5))
+# print(fruits_Price(10))
+# print(fruits_Price(15))
+
+# def fruit(price=10):
+#     print(price * 10)
+
+# fruit(150)
+# fruit()
+
+# def Man_Data(name = "na", age = 0, Father   = "na"):
+#     print("Hello", name, age, Father)
+# Man_Data("hem", 18, "shem")
+
+# Man_Data(age = 18, Father = "shem", name = "hem")
+# Man_Data()
+
+# def fruit(price, weight = 10):
+#     print("hi, price is" , price * weight)
+
+# fruit(10)
+# fruit(10, 100)
+# fruit()
+
+# for i in range(3, 10, 4):
+#     print(i)
+
+# print(list(range(3)))
+# print(list(range(3, 10)))
+# print(list(range(3, 10, 4)))
+
+# r = range(10)
+
+# print(r[2])
+# print(r[:3])
